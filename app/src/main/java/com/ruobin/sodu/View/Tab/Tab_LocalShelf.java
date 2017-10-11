61f9cf819078e3956421ea09e67c6f68bf83bcc6
ruobin <83250112@qq.com>
2017-10-11 14:48:25 +0800

    ss

--- app/src/main/java/com/ruobin/sodu/View/Tab/Tab_LocalShelf.java
+++ app/src/main/java/com/ruobin/sodu/View/Tab/Tab_LocalShelf.java
@@ -1,13 +1,20 @@
 package com.ruobin.sodu.View.Tab;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.TextView;
 
 import com.ruobin.sodu.Constants.SoDuUrl;
 import com.ruobin.sodu.Interface.IHtmlRequestResult;
+import com.ruobin.sodu.Model.Book;
 import com.ruobin.sodu.R;
+import com.ruobin.sodu.Service.ListDataAnalysisService;
+import com.ruobin.sodu.UpdateCatalogActivity;
 import com.ruobin.sodu.Util.HttpHelper;
+
+import java.util.List;
 
 
 public class Tab_LocalShelf extends BaseTabFragment {
@@ -15,28 +22,14 @@ public class Tab_LocalShelf extends BaseTabFragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setId(R.layout.fragment_tab_local_shelf,R.layout.item_rank,false);
+        super.setId(R.layout.fragment_tab_local_shelf,R.layout.item_local,false);
     }
 
 
-
-    @Override
-    public void setData(String html) {
-
-        super.setData(html);
-    }
-
-
-    @Override
-    public void onRequestFailure() {
-
-
-    }
-
-    @Override
     public void loadData() {
-
+        super.loadData();
         String url = SoDuUrl.home;
+
         HttpHelper.getHtmlByUrl(url, new IHtmlRequestResult() {
             @Override
             public void success(String html) {
@@ -48,12 +41,29 @@ public class Tab_LocalShelf extends BaseTabFragment {
                 onRequestFailure();
             }
         });
-
     }
+
+
+    @Override
+    public void setData(String html) {
+
+        if(html == null || html == ""){
+            return;
+        }
+
+        List<Book> list  = ListDataAnalysisService.AnalysisHotRecommendDatas(html);
+        books = list;
+        super.updateUI();
+    }
+
 
     @Override
     public void itemClick(View view, int position) {
-
+        Book book = books.get(position);
+        Intent intent = new Intent();
+        intent.setClass(this.getActivity(), UpdateCatalogActivity.class);
+        intent.putExtra("book", book);
+        startActivity(intent);
     }
 
     @Override
@@ -64,8 +74,16 @@ public class Tab_LocalShelf extends BaseTabFragment {
     @Override
     public void itemInitData(View view, Object item) {
 
+        TextView tv = (TextView) view.findViewById(R.id.item_local_book_name);
+        tv.setText(((Book) item).BookName);
 
+        TextView tv2 = (TextView) view.findViewById(R.id.item_local_new_chapter_name);
+        tv2.setText(((Book) item).NewestCatalogName);
+
+        TextView tv3 = (TextView) view.findViewById(R.id.item_local_old_chapter_name);
+        tv3.setText(((Book) item).LastReadCatalogName);
     }
+
 
 
 }
